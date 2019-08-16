2b321985fd4a1030c02f60f813829420f5fe0e8d
AFWebDev <andrewfariaj@gmail.com>
2019-08-16 15:41:25 -0400

    fix cards

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -12,11 +12,28 @@ $(document).ready(() => {
 				for (repo of data) {
 					if (repo.description) {
 						console.log(repo);
-						$(".repos").append(`<div class="card"><p>${repo.description}</p></div>`);
+						$("#repoRow").append(`
+					<div class="col-lg-4 col-sm-12 col-md-6 p-2 ">
+						<div class="card">
+							<div class="card-body">
+								<h5 class="card-title">${repo.name}</h5>
+								<p class="card-text">${repo.description}</p>
+							</div>
+							<ul class="list-group list-group-flush">
+								<li class="list-group-item languages">JS, HTML, CSS, Firebase, JQuery</li>
+								<li class="list-group-item">Dapibus ac facilisis in</li>
+							</ul>
+							<div class="card-body">
+								<a href="https://github.com/${repo.full_name}" class="card-link">More info</a>
+								<a href="https://afwebdev.github.io/${repo.name}" class="card-link">Deployed</a>
+
+							</div>
+						</div>
+					</div>`);
 					}
 				}
 			});
 	};
 
-	// getGit();
+	getGit();
 });
