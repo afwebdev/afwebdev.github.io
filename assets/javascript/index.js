db4938939440132112a979d1aa23b8d09570b314
AFWebDev <andrewfariaj@gmail.com>
2019-09-06 22:54:35 -0400

    clean up cards,

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -6,12 +6,12 @@ $(document).ready(() => {
 		let baseURL = "https://api.github.com";
 		let repos = "/users/afwebdev/repos";
 
-		fetch(`${baseURL}${repos}`, function() {})
+		fetch(`${baseURL}${repos}`)
 			.then(response => response.json())
 			.then(data => {
+				console.log(data);
 				for (repo of data) {
 					if (repo.description) {
-						console.log(repo);
 						$("#repoRow").append(`
 					<div class="col-lg-4 col-sm-12 col-md-6 p-2 ">
 						<div class="card">
@@ -19,10 +19,6 @@ $(document).ready(() => {
 								<h5 class="card-title">${repo.name}</h5>
 								<p class="card-text">${repo.description}</p>
 							</div>
-							<ul class="list-group list-group-flush">
-								<li class="list-group-item languages">JS, HTML, CSS, Firebase, JQuery</li>
-								<li class="list-group-item">Dapibus ac facilisis in</li>
-							</ul>
 							<div class="card-body">
 								<a href="https://github.com/${repo.full_name}" class="card-link">More info</a>
 								<a href="https://afwebdev.github.io/${repo.name}" class="card-link">Deployed</a>
